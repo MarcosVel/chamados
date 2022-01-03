9dbb3ebfa90cd3c4c6a2142cf85f0ed7eb023c36
MarcosVel <marcos.veloso@sempreceub.com>
2022-01-03 18:02:35 -0300

    add signUp and created function signOut

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import firebase from "../services/firebaseConnection";
 
 export const AuthContext = createContext({});
 
@@ -10,20 +11,60 @@ function AuthProvider({ children }) {
   useEffect(() => {
     function loadStorage() {
       const storageUser = localStorage.getItem('SistemaUser');
-      
-      if(storageUser) {
+
+      if (storageUser) {
         setUser(JSON.parse(storageUser));
         setLoading(false);
       }
-  
+
       setLoading(false);
     }
 
     loadStorage();
   }, [])
 
+  async function signUp(name, email, password) {
+    setLoadingAuth(true);
+    await firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+
+        await firebase.firestore().collection('users')
+          .doc(uid).set({
+            nome: name,
+            avatarUrl: null,
+          })
+          .then(() => {
+            let data = {
+              uid: uid,
+              nome: name,
+              email: value.user.email,
+              avatarUrl: null
+            };
+
+            setUser(data);
+            storageUser(data);
+            setLoadingAuth(false);
+          })
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoadingAuth(false);
+      })
+  }
+
+  function storageUser(data) {
+    localStorage.setItem('SistemaUser', JSON.stringify(data));
+  }
+
+  async function signOut() {
+    await firebase.auth().signOut();
+    localStorage.removeItem('SistemaUser');
+    setUser(null);
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, loading }}>
+    <AuthContext.Provider value={{ signed: !!user, user, loading, signUp, signOut }}>
       {/* !! transforma para boolean */}
       {children}
     </AuthContext.Provider>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import './styles.css'
 import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth';
 
 function SignUp() {
-  const [nome, setNome] = useState('');
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const { signUp } = useContext(AuthContext);
+
   function handleSubmit(e) {
     e.preventDefault();
-    alert('clicou')
+    
+    if(name !== '' && email !== '' && password !== '') {
+      signUp(name, email, password);
+    }
   }
 
   return (
@@ -22,7 +28,7 @@ function SignUp() {
 
         <form onSubmit={handleSubmit}>
           <h1>Crie sua conta</h1>
-          <input type="text" placeholder='Seu nome' value={nome} onChange={(e) => setNome(e.target.value)} />
+          <input type="text" placeholder='Seu nome' value={name} onChange={(e) => setName(e.target.value)} />
           <input type="text" placeholder='nome@email.com' value={email} onChange={(e) => setEmail(e.target.value)} />
           <input type="password" placeholder='****' value={password} onChange={(e) => setPassword(e.target.value)} />
           <button type="submit">Cadastrar</button>
