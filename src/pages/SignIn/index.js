04d095f4e2f8e99a17cbd05f09e81ca8fec4d6d5
MarcosVel <marcos.veloso@sempreceub.com>
2022-01-03 19:44:56 -0300

    add signIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import './styles.css'
 import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth';
 
 function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const { signIn } = useContext(AuthContext);
+
   function handleSubmit(e) {
     e.preventDefault();
-    alert('clicou')
+
+    if (email !== '' && password !== '') {
+      signIn(email, password);
+    }
   }
 
   return (
@@ -21,7 +27,7 @@ function SignIn() {
 
         <form onSubmit={handleSubmit}>
           <h1>Entrar</h1>
-          <input type="text" placeholder='nome@email.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="email" placeholder='nome@email.com' value={email} onChange={(e) => setEmail(e.target.value)} />
           <input type="password" placeholder='****' value={password} onChange={(e) => setPassword(e.target.value)} />
           <button type="submit">Acessar</button>
         </form>
