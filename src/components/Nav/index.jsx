f53469bca2e24a478a1bb032e08fdf15ec757322
MarcosVel <marcos.veloso@sempreceub.com>
2022-01-05 15:14:41 -0300

    add styles for nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,8 +1,34 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Avatar from '../../assets/avatar.png';
+import { AuthContext } from '../../contexts/auth';
 import './styles.css';
 
+import { FiHome, FiUser, FiSettings } from 'react-icons/fi'
+
 function Nav() {
+  const { user } = useContext(AuthContext);
+
   return (
-    <h1>Nav</h1>
+    <div className="sidebar">
+      <div>
+        <img src={user.avatarUrl == null ? Avatar : user.avatarUrl} alt="User avatar" />
+      </div>
+
+      <Link to='/dashboard'>
+        <FiHome color='#fff' size='24' />
+        Chamados
+      </Link>
+      <Link to='/profile'>
+        <FiUser color='#fff' size='24' />
+        Chamados
+      </Link>
+      <Link to='/settings'>
+        <FiSettings color='#fff' size='24' />
+        Chamados
+      </Link>
+    </div>
+
   )
 }
 
