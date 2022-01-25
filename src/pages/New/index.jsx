212f0364e79584c84b40a3050ab3c4544c3b1b2c
MarcosVel <marcos.veloso@sempreceub.com>
2022-01-25 20:15:29 -0300

    add useState to inputs in new call

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiPlusCircle } from 'react-icons/fi';
 import Nav from '../../components/Nav';
 import Title from '../../components/Title';
 import './styles.css';
 
 function New() {
+  const [assunto, setAssunto] = useState('Suporte');
+  const [status, setStatus] = useState('Aberto');
+  const [descricao, setDescricao] = useState('')
 
   function handleRegister(e) {
     e.preventDefault();
 
     alert('teste')
+  }
+
+  function handleChangeStatus(e) {
+    setStatus(e.target.value);
   }
 
   return (
@@ -29,30 +36,53 @@ function New() {
             </select>
 
             <label>Assunto</label>
-            <select>
-              <option key={1} value='suporte'>Suporte</option>
-              <option key={1} value='Visita tecnica'>Visita técnica</option>
-              <option key={1} value='financeiro'>Financeiro</option>
+            <select value={assunto} onChange={(e) => setAssunto(e.target.value)}>
+              <option key={1} value='Suporte'>Suporte</option>
+              <option key={2} value='Visita técnica'>Visita técnica</option>
+              <option key={3} value='Financeiro'>Financeiro</option>
             </select>
 
             <label>Status</label>
             <div className="status">
               <label className="status-label">
-                <input type="radio" name="radio" value="aberto" />
+                <input
+                  type="radio"
+                  name="radio"
+                  value="Aberto"
+                  onChange={handleChangeStatus}
+                  checked={status === 'Aberto'}
+                />
                 Em aberto
               </label>
               <label className="status-label">
-                <input type="radio" name="radio" value="progresso" />
+                <input
+                  type="radio"
+                  name="radio"
+                  value="Progresso"
+                  onChange={handleChangeStatus}
+                  checked={status === 'Progresso'}
+                />
                 Progresso
               </label>
               <label className="status-label">
-                <input type="radio" name="radio" value="atendido" />
+                <input
+                  type="radio"
+                  name="radio"
+                  value="Atendido"
+                  onChange={handleChangeStatus}
+                  checked={status === 'Atendido'}
+                />
                 Atendido
               </label>
             </div>
 
             <label id="description">Descrição</label>
-            <textarea type="text" placeholder="Descreva seu problema (opcional)"></textarea>
+            <textarea
+              type="text"
+              placeholder="Descreva seu problema (opcional)"
+              value={descricao}
+              onChange={(e) => setDescricao(e.target.value)}
+            />
 
             <button type="submit">Registrar</button>
           </form>
